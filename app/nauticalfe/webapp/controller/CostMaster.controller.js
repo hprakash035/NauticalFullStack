b5db9ab146755bfa107e44905c34850f2b6f6cd0
Himanshu Prakash <himanshu.shila035@gmail.com>
2023-12-20 06:32:41 +0000

    change 2

--- app/nauticalfe/webapp/controller/CostMaster.controller.js
+++ app/nauticalfe/webapp/controller/CostMaster.controller.js
@@ -1,17 +1,87 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/routing/History"
     ],
-    function(BaseController) {
+    function(Controller,History) {
       "use strict";
   
-      return BaseController.extend("nauticalfe.controller.CostMaster", {
+      return Controller.extend("nauticalfe.controller.CostMaster", {
         onInit() {
         },
+        
         onBackPress: function () {
             const oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("MastView");
-          }
+          },newEntries: function () {
+            this.getView().byId("createTypeTable").setVisible(false)
+            this.getView().byId("entryTypeTable").setVisible(true)
+            this.getView().byId("mainPageFooter").setVisible(true)
+    
+    
+          },onSave: function () {
+    
+            var value1 =  this.getView().byId("COSTCODE").getValue();
+            var value2 =  this.getView().byId("CSTCODES").getValue();
+    
+    
+            
+            var data = {
+    
+              COSTCODE: value1,
+    
+              CSTCODES: value2
+    
+            };
+            console.log(data);
+    
+    
+            
+            var JsonData = JSON.stringify(data)
+            let EndPoint = "/odata/v4/nautical/NAVOYGC";
+            fetch(EndPoint, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JsonData
+            })
+              .then(function (res) {
+                
+                if (res.ok) {
+                                    console.log("Entity created successfully");
+                  MessageToast.show(`Entity created successfully`)
+                 
+    
+                }
+                else {
+                  console.log("Failed");
+                  MessageToast.show(`Failed`)
+                }
+              })
+              .catch(function (err) {
+                console.log("error", err);
+              })
+              this.getView().byId("createTypeTable").setVisible(true)
+              this.getView().byId("entryTypeTable").setVisible(false)
+              this.getView().byId("mainPageFooter").setVisible(false)
+              this.getView().getModel().refresh();
+           },
+           backPress: function () {
+             const oHistory = History.getInstance();
+             const sPreviousHash = oHistory.getPreviousHash();
+     
+             if (sPreviousHash !== undefined) {
+               window.history.go(-1);
+             } else {
+               const oRouter = this.getOwnerComponent().getRouter();
+               oRouter.navTo("MastView", {}, true);
+             }
+           }, onExit: function () {
+             const oRouter = this.getOwnerComponent().getRouter();
+             oRouter.navTo("MastView");
+           }
+     
       });
     }
   );
--- app/nauticalfe/webapp/controller/EventMaster.controller.js
+++ app/nauticalfe/webapp/controller/EventMaster.controller.js
@@ -1,17 +1,87 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/routing/History",
+        "sap/m/MessageToast"
+
     ],
-    function(BaseController) {
+    function(Controller,History,MessageToast) {
       "use strict";
   
-      return BaseController.extend("nauticalfe.controller.EventMaster", {
+      return Controller.extend("nauticalfe.controller.EventMaster", {
         onInit() {
         },
         onBackPress: function () {
-            const oRouter = this.getOwnerComponent().getRouter();
-            oRouter.navTo("MastView");
-          }
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("MastView");
+        },newEntries: function () {
+          this.getView().byId("createTypeTable").setVisible(false)
+          this.getView().byId("entryTypeTable").setVisible(true)
+          this.getView().byId("mainPageFooter").setVisible(true)
+  
+  
+        },onSave: function () {
+  
+          var value1 =  this.getView().byId("EVTTY").getValue();
+          var value2 =  this.getView().byId("TEXT").getValue();
+  
+  
+          
+          var data = {
+  
+            EVTTY: value1,
+  
+            TEXT: value2
+  
+          };
+          console.log(data);
+  
+  
+          
+          var JsonData = JSON.stringify(data)
+          let EndPoint = "/odata/v4/nautical/EVENT_MAS";
+          fetch(EndPoint, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JsonData
+          })
+            .then(function (res) {
+              
+              if (res.ok) {
+                console.log("Entity created successfully");
+                MessageToast.show(`Entity created successfully`)
+               
+  
+              }
+              else {
+                console.log("Failed");
+                MessageToast.show(`Failed`)
+              }
+            })
+            .catch(function (err) {
+              console.log("error", err);
+            })
+            this.getView().byId("createTypeTable").setVisible(true)
+            this.getView().byId("entryTypeTable").setVisible(false)
+            this.getView().byId("mainPageFooter").setVisible(false)
+            this.getView().getModel().refresh();
+         },
+         backPress: function () {
+           const oHistory = History.getInstance();
+           const sPreviousHash = oHistory.getPreviousHash();
+   
+           if (sPreviousHash !== undefined) {
+             window.history.go(-1);
+           } else {
+             const oRouter = this.getOwnerComponent().getRouter();
+             oRouter.navTo("MastView", {}, true);
+           }
+         }, onExit: function () {
+           const oRouter = this.getOwnerComponent().getRouter();
+           oRouter.navTo("MastView");
+         }
       });
     }
   );
